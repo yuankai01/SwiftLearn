9b94a5226c636d1a4d6c303cbb49779c741df839
gaohuifeng <735285388@qq.com>
2019-09-06 15:34:05 +0800

    测试

--- SwiftStudy/SwiftStudy/CustomView/SwiftAlignButton.swift
+++ SwiftStudy/SwiftStudy/CustomView/SwiftAlignButton.swift
@@ -30,26 +30,41 @@ struct AlignType {
     
 }
 
+struct Circle {
+    var radius: Double
+}
+
+
 class SwiftAlignButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 //        let strucdd = AlignType()
-//
+        
 //
 //        print("top = \(strucdd.Top)")
+        
+        var circleA = Circle(radius: 5.0)
+        let circleB = circleA
+        circleA.radius = 10
+        print(circleA)
+        print(circleB)
+        
+        print("magenta ==\(magenta)")
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
  
+    //函数体外 竟然可以定义变量？？？，但是并不能打印print
     let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
     let halfGray = Color(white: 0.5)
     let veryGreen = Color(red: 0.0, green: 1.0, blue: 0.0)
     let test = Color.init(white: 1)
     var aa = Color(white: 3)
     let df = Color(red: 13, green: 2, blue: 1)
+    
     
 }
 
--- SwiftStudy/SwiftStudy/ViewController.swift
+++ SwiftStudy/SwiftStudy/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
+enum AlignTypedaa : Int {
+    case top = 10,left,right,bottom
+    case aa
+    //    case Left,
+    //    case Right,
+    //    case Bottom
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ViewController  viewDidLoad ==")
+        let align = AlignTypedaa.bottom.rawValue
+        
+        print("align == \(String(describing: align))")
+        
+        let alignBtn = SwiftAlignButton.init(frame: CGRect.init(x: 20, y: 100, width: 100, height: 100))
+        alignBtn.backgroundColor = .red
+        self.view.addSubview(alignBtn)
     }
-
-
+    
 }
 
